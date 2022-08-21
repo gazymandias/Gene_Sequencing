bd1c43416916f47bb9eb6838f9dd9b6ea29c7dda
Gareth Cadman <48953362+gazymandias@users.noreply.github.com>
2022-08-21 16:37:09 +0100

    Update gene_sequence.go
    Added support for concurrency and generation of dummy data.

--- gene_sequence.go
+++ gene_sequence.go
@@ -2,20 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
 	"strings"
+	"time"
+	
 )
 
 func main() {
-	var genetic_code = "AATCCGCTAGATCGATCTCCAGGATTTATACCGGTACA"
-	if !basesOnly(genetic_code) {
-		fmt.Println("Please input a valid genetic sequence.")
-	} else {
-		sequence(genetic_code)
+	start := time.Now()
+	n := 0
+	for n < 1000000 {
+		// var genetic_code = "AATCCGCTAGATCGATCTCCAGGATTTATACCGGTACA"
+		var genetic_code = DummyGeneticCode(1000)
+		if !basesOnly(genetic_code) {
+			fmt.Println("Please input a valid genetic sequence.")
+		} else {
+			go sequence(genetic_code)
+		}
+		n += 1
 	}
+	elapsed := time.Since(start)
+	log.Printf("Process took %s", elapsed)
 }
 
 // dna uses 't' while rna uses 'u'
 const bases = "acgt"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func DummyGeneticCode(length int) string {
+	return StringWithCharset(length, bases)
+}
 
 func basesOnly(s string) bool {
 	for _, char := range s {
