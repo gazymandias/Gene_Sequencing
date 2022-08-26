c98806d9df1f976945c79bcef6230a70c1adc558
Gareth Cadman <48953362+gazymandias@users.noreply.github.com>
2022-08-26 17:06:19 +0100

    Update gene_sequence.go

--- gene_sequence.go
+++ gene_sequence.go
@@ -3,27 +3,23 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
 	"math/rand"
 	"strings"
 	"time"
 )
 
 func main() {
-	start := time.Now()
 	n := 0
-	for n < 1 {
-		// var genetic_code = "ggtacaaccccgaatgaaacggcctcatccccccttgctcatacggatctcatttgcctccttgtgatctcgcataggacagtggtattcttggccccacgaaataccctgggtttggtattaacagtgacacgggaagcgatctattctgggctcaccgtccatccaatctgtggagggacgcgggataacacgtgcggcctaccaacagaaaatcgccggcagatcgaatgaggagcgcgagtaagaagacctgtccggctacttctaggctcagcctgataggggagacacctgaggtataataatttacttgtgaatgctgctatcgcttacgttacgttatagacctcttctgctttggattctataacgttaacacctgcgcgaggatgttgagtcaggagagggttaataccccggataggacatttgcgagcatttccgatgagctacgctgcgatcaaagcgaagaccgcgttgggggtttatgcaggacgtcgcggagattacgatagagcgagcgttttatgtgcaagcgcatgtgggatgtacacgatgtgcgtacgagcccctacctccaatatgcttcccgcaacacgatgatgagtccgacaggcgttcaccgtataaattaccatgaggttggagtacctgacgacgtaaattgaacacccataagtcgagcctaacgcgggaacgggatacctcgcgacgaaaagcgacttatcattctttcgtctgcgttcatcgttacacaagacaaaattcatccgctggctcattccattgattatgtaaggtaactgtcatggcacttttaccccatggtacacgtttgctttgtgccacactcccaatagggttcttcgaaattctcgggcgggggtgatccatatgacacatggatggaattgcaacgggccccctaataggcgcataattaataatatcatgtttacctggccccccaaacgaaatttcggtctttgcttatgaa"
-		var genetic_code = DummyGeneticCode(1000)
+	for n < 10000000 {
+		// var genetic_code = "cgcagacgcgcaagcagattttataaagccaatgtatatacaactcctaccaggccaaacaaggtccgcgcacacgaaacctggaaaactttgtgtcggc"
+		var genetic_code = DummyGeneticCode(50)
 		if !basesOnly(genetic_code) {
 			fmt.Println("Please input a valid genetic sequence.")
 		} else {
-			go sequence(genetic_code)
+			go fmt.Println(sequence(genetic_code))
 		}
 		n += 1
 	}
-	elapsed := time.Since(start)
-	log.Printf("Process took %s", elapsed)
 }
 
 // dna uses 't' while rna uses 'u'
@@ -61,9 +57,15 @@ func findSequenceRange(s string) (int, int, error) {
 	stop_codons := []int{stop_codon_one, stop_codon_two, stop_codon_three}
 	stop_codon := stop_codons[0]
 	for _, v := range stop_codons {
-		if v < stop_codon && v > start_codon {
+		if (v < stop_codon || stop_codon < 0) && v > start_codon {
 			stop_codon = v
 		}
+	}
+	if start_codon == -1 {
+		return -1, -1, errors.New("no valid start codon")
+	}
+	if stop_codon < start_codon {
+		return -1, -1, errors.New("start codon is after stop codon")
 	}
 	if stop_codon == 0 {
 		return -1, -1, errors.New("no valid stop codon after start codon")
@@ -71,23 +73,26 @@ func findSequenceRange(s string) (int, int, error) {
 	return start_codon, stop_codon, nil
 }
 
-func cleanSequenceRange(s string) string {
+func cleanSequenceRange(s string) (string, error) {
 	start_codon, stop_codon, err := findSequenceRange(s)
 	if err != nil {
-		fmt.Println("error:", err)
+		return s, err
 	}
 	s = s[start_codon:][0 : (stop_codon+3)-start_codon]
-	return s
+	return s, nil
 }
 
-func sequence(s string) {
+func sequence(s string) (string, map[string]int, error) {
 	type sequence struct {
 		codon string
 		count int
 	}
-
+	var o = s
 	var m = make(map[string]int)
-	s = cleanSequenceRange(s)
+	s, err := cleanSequenceRange(s)
+	if err != nil {
+		return o, m, err
+	}
 	var codons_to_sequence int = 0
 	if len(s)%3 == 0 {
 		codons_to_sequence = len(s) / 3
@@ -100,6 +105,5 @@ func sequence(s string) {
 		s = s[3:]
 		codons_to_sequence -= 1
 	}
-	fmt.Println(m)
+	return o, m, err
 }
-
